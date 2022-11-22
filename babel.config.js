3ae5333510976555ed2cd8e23646a6a47653a80c
Casan <whdrjsco@gmail.com>
2022-11-22 22:19:57 +0900

    Template babel-plugin-module-resolver 적용(Custom Path Alias)

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          api: ['./src/api'],
+          assets: ['./src/assets'],
+          components: ['./src/components'],
+          global: ['./src/global'],
+          screen: ['./src/screen'],
+          store: ['./src/store'],
+        },
+      },
+    ],
+  ],
 };
